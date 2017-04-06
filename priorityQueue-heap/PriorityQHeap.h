46ea4725c75087d106e40a46c9498109e85177d0
Hernan Iruegas <hernaniruegas@hotmail.com>
2017-04-06 01:24:51 -0500

    push and pop functions for priority queue with heap

--- priorityQueue-heap/PriorityQHeap.h
+++ priorityQueue-heap/PriorityQHeap.h
@@ -5,38 +5,87 @@ class PriorityQHeap{
 		PriorityQHeap(bool priority);
 		void push(int num);
 		void pop();
-		void top();
+		int top();
 		int size();
 		bool empty();
+		void print();
 
 	private:
-		vector<int> v;
+		int a[101];
 		bool priority;
 		int howMany;
 };
 
 PriorityQHeap::PriorityQHeap(){
-	vector<int> v(101);
 	priority = true;
 	howMany = 0;
 }
 
 PriorityQHeap::PriorityQHeap(bool priority){
-	vector<int> v(101);
 	this->priority = priority;
 	howMany = 0;
 }
 
 void PriorityQHeap::push(int num){
-	
+	if(howMany==0){
+		a[howMany+1] = num;
+		howMany++;
+		return;
+	}
+	if(a[(howMany+1)/2] > num){
+		a[howMany+1] = num;
+		howMany++;
+		return;
+	}else{
+		//hacemos el intercambio de los valores en el arreglo
+		int temp = a[(howMany+1)/2];
+		a[(howMany+1)/2] = num;
+		a[howMany+1] = temp;
+
+		int position = (howMany+1)/2; //lugar en donde está el # que cambio de casilla
+		for(int i = position; i>1; i/=2){
+			if(a[position] < a[position/2])
+				return;
+			else{
+				//hacemos el intercambio de los valores en el arreglo
+				temp = a[position/2];
+				a[position/2] = a[position];
+				a[position] = temp;
+				position = position/2;
+			}
+		}
+		howMany++;
+	}
 }
 
 void PriorityQHeap::pop(){
-	
+	//pones al hijo mayor como el sucesor y recorres todo el arreglo haciendo esto
+
+	int aux = 1;
+	howMany--;
+
+	if(a[aux*2] > a[(aux*2)+1]){
+		a[aux] = a[aux*2];
+		aux = aux*2;
+	}else{
+		a[aux] = a[(aux*2)+1];
+		aux = (aux*2)+1;
+	}
+
+	for(int i=aux; i<howMany; i*=aux){
+		cout<<"hola"<<endl;
+		if(a[aux*2] > a[(aux*2)+1]){
+			a[aux] = a[aux*2];
+			aux = aux*2;
+		}else{
+			a[aux] = a[(aux*2)+1];
+			aux = (aux*2)+1;
+		}
+	}
 }
 
-void PriorityQHeap::top(){
-	
+int PriorityQHeap::top(){
+	return a[1];
 }
 
 int PriorityQHeap::size(){
@@ -45,6 +94,11 @@ int PriorityQHeap::size(){
 
 bool PriorityQHeap::empty(){
 	return howMany == 0;
+}
+
+void PriorityQHeap::print(){
+	for(int i=1; i<=howMany; i++)
+		cout<<a[i]<<" ";
 }
 
 
--- priorityQueue-heap/main.cpp
+++ priorityQueue-heap/main.cpp
@@ -1,9 +1,22 @@
-#include <vector>
+#include <iostream>
 using namespace std;
 #include "PriorityQHeap.h"
 
 int main(){
 
+	PriorityQHeap pqh;
+	
+	pqh.push(19);
+	pqh.push(29);
+	pqh.push(5);
+	pqh.push(10);
+	pqh.push(70);
+
+	cout<<pqh.top()<<endl;
+	pqh.print();
+	cout<<endl;
+	pqh.pop();
+	pqh.print();
 	return 0;
 }
 
